71ff960561d0df8c17ca73444c42086487c1ddb4
rowend36 <rowendduke36@gmail.com>
2023-03-31 18:33:00 +0100

    wrote 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,27 +1,50 @@
-#include "main.h"
-
+#include "stdlib.h"
+#include "stdio.h"
 /**
  * rot13 - encodes a string in rot13
  * @s: string to be encoded
  * Return: the resulting strring
  */
-char *rot13(char *s)
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j;
+int a = strlen(n1);
+int b = strlen(n2);
+int carry = 0;
+int i = size_r - 1;
+while (a > 0 && b > 0 && i > 0)
+{
+	char acc = carry;
 
-	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-
-	for (i = 0; s[i] != '\0'; i++)
+	if (a > 0)
 	{
-		for (j = 0; a[j] != '\0'; j++)
-		{
-			if (s[i] == a[j])
-			{
-				s[i] = b[j];
-				break;
-			}
-	    }
+		acc += *(n1 + a - 1) - 48;
 	}
-	return (s);
+
+	if (b > 0)
+	{
+		acc += *(n2 + b - 1) - 48;
+	}
+
+	if (acc >= 10)
+	{
+		acc -= 10;
+		carry = 1;
+	} else
+	{
+		carry = 0;
+	}
+	*(r + i - 1) = acc + 48;
+	i -= 1;
+	a -= 1;
+	b -= 1;
+}
+
+if (i > 0 || a == 0 && b == 0)
+{
+	*(r + size_r - 1) = '\x0';
+	return (r + i);
+} else
+{
+	return (0);
+}
 }
