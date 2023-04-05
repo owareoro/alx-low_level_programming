548294d9703c9138ca2a3cda0cf9a177a822ca54
rowend36 <rowendduke36@gmail.com>
2023-04-05 11:23:17 +0100

    test mobile

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -16,7 +16,9 @@ int main(void)
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
-  *(p - 4) = 98;
+  printf("%p\n", p);
+  printf("%p\n", &a);
+  printf("%p\n", &a[1]);
   /* ...so that this prints 98\n */
   printf("a[2] = %d\n", a[2]);
   return (0);
