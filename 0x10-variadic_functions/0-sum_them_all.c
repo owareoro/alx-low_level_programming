793892a5846d6e458f83353ec192c139f8175234
rowend36 <rowendduke36@gmail.com>
2023-04-21 06:24:27 +0100

    still trying

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,8 +11,13 @@ int sum = 0;
 unsigned int i;
 va_list arguments;
 va_start(arguments, n);
+
+if (n == 0)
+	return (0);
+
 for (i = 0; i < n; i++)
-    sum += va_arg(arguments, int);
+	sum += va_arg(arguments, int);
 va_end(arguments);
+
 return (sum);
 }
